228c4f774c710770cbac9b1f509e54712d23d44e
Egoorbis <egoorbis@users.noreply.github.com>
2026-02-17 15:50:35 +0100

    Refactor GitHub app authentication variables in create-project.yml and providers.tf

--- projects/providers.tf
+++ projects/providers.tf
@@ -13,10 +13,7 @@ terraform {
 
 provider "github" {
     owner = var.github_organization
-    
+
     app_auth {
-      id              = var.github_app_client_id
-      installation_id = var.github_app_installation_id
-      pem_file        = var.github_app_pem_content
     }
 }
