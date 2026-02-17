1ff130eb5e67175f13269f14a1d7fac060c2c2c2
Egoorbis <egoorbis@users.noreply.github.com>
2026-02-17 15:51:25 +0100

    Remove GitHub app variables from variables.tf

--- projects/variables.tf
+++ projects/variables.tf
@@ -4,20 +4,6 @@ variable "github_organization" {
   default = "Egoorbis"
 }
 
-variable "github_app_client_id" {
-  type        = string
-  description = "The App ID of the GitHub App (found on the App General page)"
-}
-
-variable "github_app_installation_id" {
-  type        = string
-  description = "The Installation ID of the GitHub App"
-}
-
-variable "github_app_pem_content" {
-  type      = string
-  sensitive = true
-}
 
 variable "azure_sp_json" {
   type      = string
