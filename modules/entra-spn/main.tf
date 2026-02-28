4fcf3bca5c4ec853cacc20ee08032a7e17edd1cd
Egoorbis <egoorbis@users.noreply.github.com>
2026-02-28 14:02:08 +0100

    feat: add GitHub Actions workflow for Terraform project management

--- modules/entra-spn/main.tf
+++ modules/entra-spn/main.tf
@@ -1,14 +1,11 @@
-# 1. Create the Entra ID Application
 resource "azuread_application" "this" {
   display_name = "spn-${var.repo_name}"
 }
 
-# 2. Create the Service Principal
 resource "azuread_service_principal" "this" {
   client_id = azuread_application.this.client_id
 }
 
-# 3. Create the Federated Credential (OIDC Trust)
 resource "azuread_application_federated_identity_credential" "this" {
   application_id = azuread_application.this.id
   display_name   = "github-actions-oidc"
@@ -18,7 +15,6 @@ resource "azuread_application_federated_identity_credential" "this" {
   subject        = "repo:${var.repo_name}:ref:refs/heads/main"
 }
 
-# 4. Scope-based Role Assignment (Contributor on Subscription)
 resource "azurerm_role_assignment" "this" {
   scope                = "/subscriptions/${var.azure_subscription_id}"
   role_definition_name = "Contributor"
